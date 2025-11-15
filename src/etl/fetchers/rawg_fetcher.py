0c879027c884c8973d204e9ced0f5a916e9e97fa
krn <contadorkrn@gmail.com>
2025-11-15 12:20:49 -0300

    DEV: Finished development of the first iteration of the RAWG fetcher

--- src/etl/fetchers/rawg_fetcher.py
+++ src/etl/fetchers/rawg_fetcher.py
@@ -2,11 +2,14 @@
 File for development of the RAWG fetcher - gets raw data from the RAWG API
 """
 
+import json
 import os
 import requests
+import datetime
 
 
 from dotenv import load_dotenv
+from pathlib import Path
 
 # region ------------ Load environment variables ------------
 load_dotenv()
@@ -21,8 +24,27 @@ headers: dict = {"accept": "application/json"}
 # endregion
 
 
-if __name__ == "__main__":
-    response: requests.Response = requests.get(test_url, headers=headers)
+# region ------------ Get project path ------------
+PROJECT_ROOT: Path = Path(__file__).parent.parent.parent.parent
+DATA_LOCAL: Path = PROJECT_ROOT / "data_local" / "raw"
+DATA_LOCAL.mkdir(parents=True, exist_ok=True)
 
+# endregion
+
+
+if __name__ == "__main__":
+    response: requests.Response = requests.get(main_url, headers=headers)
+    data: json = response.json()
+
+    os.makedirs("data_local/raw", exist_ok=True)
+
+    filename: str = (
+        DATA_LOCAL
+        / f"rawg_response_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json"
+    )
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+    print(f"Saved RAWG response to {filename}")
     print(response.status_code)
-    print(response.json())
