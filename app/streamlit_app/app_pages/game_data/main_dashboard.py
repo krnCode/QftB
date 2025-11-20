ecefb52cc0a5efe42928b56b2fe3f9a5a22492e0
krn <contadorkrn@gmail.com>
2025-11-20 15:19:56 -0300

    DEV: Started developing overview page. For now it reads the parquet file saved locally.

---
+++ app/streamlit_app/app_pages/game_data/main_dashboard.py
@@ -0,0 +1,63 @@
+"""
+File for the main dashboard of the game data page
+"""
+
+import os
+import streamlit as st
+import polars as pl
+
+from pathlib import Path
+
+st.set_page_config(page_title="Game Data", layout="wide")
+
+# region ------------ Get project path ------------
+PROJECT_ROOT: Path = Path(__file__).parent.parent.parent.parent.parent
+DATA_LOCAL: Path = PROJECT_ROOT / "data_local" / "temp" / "rawg"
+# endregion
+
+# region ------------ Get recent data ------------
+# Get all json files in the folder and sort them by most recent
+files: list[Path] = [
+    os.path.join(DATA_LOCAL, f)
+    for f in os.listdir(DATA_LOCAL)
+    if f.endswith(".parquet")
+]
+
+files.sort(key=os.path.getmtime, reverse=True)
+latest_file: Path = files[0]
+# endregion
+
+
+# region ------------ Read parquet ------------
+data: pl.DataFrame = pl.read_parquet(latest_file)
+# endregion
+
+
+# region ------------ Columns config ------------
+columns_config: dict = {
+    "game_id": st.column_config.TextColumn(label="Game ID"),
+    "slug": st.column_config.TextColumn(label="Slug"),
+    "name": st.column_config.TextColumn(label="Name", pinned=True),
+    "released": st.column_config.DateColumn(label="Released", format="DD/M/YYYY"),
+    "rating": st.column_config.NumberColumn(label="Rating", pinned=True),
+    "ratings_count": st.column_config.NumberColumn(label="Ratings Count"),
+    "platforms": st.column_config.ListColumn(label="Platforms"),
+    "genres": st.column_config.ListColumn(label="Genres"),
+    "updated_at": st.column_config.DateColumn(label="Updated At", format="DD/M/YYYY"),
+}
+# endregion
+
+
+# region ------------ App ------------
+st.title("Game Data")
+st.write("Data from the RAWG API: https://rawg.io/")
+st.write("---")
+
+if data.height > 0:
+    st.dataframe(
+        data=data,
+        column_config=columns_config,
+        hide_index=True,
+        selection_mode=["multi-cell", "multi-column"],
+    )
+# endregion
