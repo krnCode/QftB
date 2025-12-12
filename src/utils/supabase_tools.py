64c2289cd3a76dc3559911ad4c466c5a1e11fad3
krn <contadorkrn@gmail.com>
2025-12-12 11:23:24 -0300

    DEV: Included new functions to be used in other scripts (querying data, create connections)

--- src/utils/supabase_tools.py
+++ src/utils/supabase_tools.py
@@ -3,13 +3,43 @@ File to handle functions that interact with the supabase database and tables
 """
 
 import polars as pl
+import os
 
 from pathlib import Path
+from supabase import create_client, Client
+from dotenv import load_dotenv
 from src.utils.supabase_client import supabase
 from src.utils.logger import setup_logger
 
+
+# region ------------ Load env variables ------------
+load_dotenv()
+
+SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+
+# endregion
+
+
 # region ------------ Logger setup ------------
 logger = setup_logger(__name__)
+# endregion
+
+
+# region ------------ Create connection ------------
+def init_connection():
+    """
+    Runs the create_client function of supabase that returns a connection to the
+    database
+
+    Returns:
+        Client: Instance of the client that connects to the database
+    """
+    url = SUPABASE_URL
+    key = SUPABASE_KEY
+    return create_client(url, key)
+
+
 # endregion
 
 
@@ -60,6 +90,39 @@ def update_table(table_name: str, data_to_update: pl.DataFrame):
     response = supabase.table(table_name).upsert(rows, on_conflict="game_id").execute()
     logger.info("Updated table: %s", table_name)
     logger.info("Number of rows updated: %s", len(rows))
+
+
+# endregion
+
+
+# region ------------ Table Query ------------
+# Query all data from rawg_games_cleaned
+def query_all_data_rawg_games_cleaned() -> list[dict]:
+    """
+    Function to paginate all the data available on the table "rawg_games_cleaned"
+
+    Returns:
+        list[dict]: List of dictionaries with all the data
+    """
+    all_results: list[dict] = []
+    batch_size: int = 1000
+    start = 0
+
+    while True:
+        response = (
+            supabase.table("rawg_games_cleaned")
+            .select("*")
+            .range(start=start, end=start + batch_size - 1)
+            .execute()
+        )
+
+        data: list[dict] = response.data
+        if not data:
+            break
+        all_results.extend(data)
+        start += batch_size
+
+    return all_results
 
 
 # endregion
