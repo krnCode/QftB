913bdfc316a69674673a63e397aa2fc514ef8a91
krn <contadorkrn@gmail.com>
2026-04-13 18:32:08 -0300

    FEAT: Start developing staging and datamarts with dbt

---
+++ dbt_qftb/models/marts/mart_games.sql
@@ -0,0 +1,53 @@
+with games as (
+    select * from {{ ref("stg_rawg__games") }}
+),
+
+details as (
+    select * from {{ ref("stg_rawg__game_details") }}
+),
+
+final as (
+    select
+        -- identifiers
+        g.game_id,
+        g.slug,
+        g.name,
+
+        -- release info
+        g.released,
+        d.tba,
+
+        -- ratings and reviews
+        g.rating,
+        g.ratings_count,
+        d.reviews_count,
+        d.reviews_text_count,
+
+        --categorisation
+        g.platforms,
+        g.genres,
+        d.esrb_rating_id,
+
+        -- engagement
+        d.achievements_count,
+
+        --media and community
+        d.background_image,
+        d.reddit_name,
+        d.reddit_url,
+
+        -- relationships (id arrays)
+        d.developer_id,
+        d.publisher_id,
+        d.tag_id,
+
+        -- metadata
+        d.updated_on_rawg,
+        g.updated_at
+
+    from games g
+    left join
+        details d using (game_id)
+)
+
+select * from final
