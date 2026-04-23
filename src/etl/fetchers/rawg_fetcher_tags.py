1b58a2def4ed56fa7f0239f37437fc7d6aa3f2ce
pauloSan <61383309+krnCode@users.noreply.github.com>
2026-04-23 19:24:52 -0300

    FEAT: tag fetcher (#21)
    * FEAT: Develop fetchet to get tags data from RAWG
    
    * STYLE: Adjust some code regions names
    
    * DEV: Develop functions to query tags on Supabase
    
    * FIX: Change the table used in the function query_existing_tags_ids
    
    * DEV: Develop functions to query tags on Supabase
    
    * FEAT: Finish development of tag fetcher

--- src/etl/fetchers/rawg_fetcher_tags.py
+++ src/etl/fetchers/rawg_fetcher_tags.py
@@ -12,6 +12,8 @@ import time
 import json
 import asyncio
 import aiohttp
+import math
+
 import polars as pl
 
 from pathlib import Path
@@ -41,8 +43,13 @@ supabase = init_connection()
 
 # region ------------ API access config ------------
 API_KEY: str = os.getenv("RAWG_GAME_DETAILS_API_KEY")
+BASE_URL_TAGS: str = f"https://api.rawg.io/api/tags"
 HEADERS: dict = {"accept": "application/json"}
-BASE_PARAMS: dict = {"key": API_KEY}
+PAGE_SIZE: int = 100
+BASE_PARAMS: dict = {
+    "key": API_KEY,
+    "page_size": PAGE_SIZE,
+}
 # endregion
 
 
@@ -53,138 +60,128 @@ DATA_LOCAL.mkdir(parents=True, exist_ok=True)
 # endregion
 
 
-async def fetch_tags(
+async def fetch_tags_page(
     session: aiohttp.ClientSession,
+    page: int,
     semaphore: asyncio.Semaphore,
-    tag_id: int,
+    # tag_id: int,
     max_retries: int = 3,
-) -> dict | None:
+) -> list[dict] | None:
     """
-    Fetches tags data from the RAWG API asynchronously.
+    Fetches pages with tags data from the RAWG API asynchronously.
 
     Args:
         session (aiohttp.ClientSession): The aiohttp session object.
+        page (int): The page number to fetch.
         semaphore (asyncio.Semaphore): The semaphore object to limit concurrency.
+        tag_id (int): The tag id to fetch data for.
         tag_id (int): The tag id to fetch.
         max_retries (int, optional): The maximum number of retries. Defaults to 3.
     """
-    url: str = f"https://api.rawg.io/api/tags/{tag_id}"
+    # url: str = f"https://api.rawg.io/api/tags/{tag_id}"
+    params: dict = BASE_PARAMS.copy()
+    params["page"] = page
 
     async with semaphore:
         for attempt in range(max_retries):
             try:
                 async with session.get(
-                    url, headers=HEADERS, params=BASE_PARAMS
+                    BASE_URL_TAGS, headers=HEADERS, params=params
                 ) as response:
                     response.raise_for_status()
                     data: dict = await response.json()
-                    return data
+                    logger.info("Successfully fetched tags page %s", page)
+                    return data.get("results", [])
 
             except (aiohttp.ClientError, asyncio.TimeoutError, ValueError) as e:
-                if attempt == max_retries - 1:
-                    logger.error("----- FINAL FAILURE FOR TAGS -----")
-                    return None
-
-                sleep_time = 2 ** (attempt + 1)
-                await asyncio.sleep(sleep_time)
-                logger.error(
-                    "Attempt %s failed for tags, retrying in %s seconds...",
+                logger.warning(
+                    "Attempt %s/%s failed for tags page %s: %s",
                     attempt + 1,
-                    sleep_time,
+                    max_retries,
+                    page,
+                    e,
                 )
+                if attempt == max_retries - 1:
+                    logger.error(
+                        "----- MAX RETRIES REACHED - FINAL FAILURE FOR TAGS PAGE %s -----",
+                        page,
+                    )
+                    return []
+                sleep_time = 2 ** (attempt + 1)
+                logger.info("Retrying tags page %s in %s seconds...", page, sleep_time)
+                await asyncio.sleep(sleep_time)
 
 
 async def main():
     """
-    Main function to run the fetcher.
+    Main function to run the tags fetcher.
     """
     start_time = time.time()
     logger.info(
-        "----- STARTED FETCHER ROUTINE AT %s -----",
+        "----- STARTED TAGS FETCHER ROUTINE AT %s -----",
         datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     )
+    timeout = aiohttp.ClientTimeout(total=30)
+    semaphore = asyncio.Semaphore(5)
 
-    # region ------------ Get ID for all tags ------------
-    logger.info("Querying Supabase for tags IDs...")
-    all_tags: list[dict] = query_all_data_rawg_tags()
-
-    # if not all_tags:
-    #     logger.warning("No tags found in Supabase, exiting...")
-    #     return
-
-    # region ------------ Create a Delta Load for fetching tags ------------
-    logger.info("Querying Supabase for existing tags IDs...")
-    existing_tags_ids: list[int] = query_existing_tags_ids()
-
-    df_all: pl.DataFrame = pl.DataFrame(data=all_tags, infer_schema_length=None)
-    all_tags_ids: list[int] = [int(x) for x in df_all["tag_id"].to_list()]
-
-    existing_set = set([int(x) for x in existing_tags_ids])
-    logger.info("Total tags in rawg_tags: %s", len(all_tags_ids))
-    logger.info("Total tags with game details in rawg_tags: %s", len(existing_set))
-
-    tags_ids: list[int] = [
-        tag_id for tag_id in all_tags_ids if tag_id not in existing_set
-    ]
-    # endregion
-
-    # region ------------ Limit requests for testing ------------
-    # SECURITY LOCK: Limit the number of requests for testing
-    # Remove or adjust when running in production
-    LIMIT_REQUESTS: int = 5  # TEST: Use this in testing
-    # LIMIT_REQUESTS: int = len(tags_ids)  # PROD: Use this in production
-    tags_ids = tags_ids[:LIMIT_REQUESTS]
-    # endregion
-
-    total_tags: int = len(tags_ids)
-    logger.info("Total tags to fetch in this run: %s", total_tags)
-    # endregion
-
-    if total_tags == 0:
-        logger.info(
-            "----- ALL TAGS ARE UP TO DATE! No new tags to fecth. Exiting... -----"
-        )
-
-        return
-
-    # region ------------ Async fetching and continuous saving ------------
-    timeout = aiohttp.ClientTimeout(total=15)
-    semaphore = asyncio.Semaphore(10)
-
-    time_now: str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename: Path = DATA_LOCAL / f"rawg_tags_response_{time_now}.jsonl"
-
-    tags_fetched: int = 0
+    all_results: list = []
 
     async with aiohttp.ClientSession(timeout=timeout) as session:
-        tasks: list[asyncio.Task] = [
-            fetch_tags(session, semaphore, tag_id) for tag_id in tags_ids
-        ]
-        logger.info("Fetching data and saving continuously to %s...", filename)
+        logger.info("Fetching first tags page to determine total count...")
 
-        with open(filename, "a", encoding="utf-8") as f:
-            for coro in asyncio.as_completed(tasks):
-                result = await coro
+        try:
+            params_p1 = BASE_PARAMS.copy()
+            params_p1["page"] = 1
+            async with session.get(
+                BASE_URL_TAGS, headers=HEADERS, params=params_p1
+            ) as response:
+                response.raise_for_status()
+                first_page_data: dict = await response.json()
 
-                if result is not None:
-                    f.write(json.dumps(result, ensure_ascii=False) + "\n")
-                    f.flush()
+                total_tags: int = first_page_data.get("count")
+                first_page_results: list = first_page_data.get("results", [])
+                all_results.extend(first_page_results)
 
-                    tags_fetched += 1
+                logger.info("Total tags for this query: %s", total_tags)
 
-                    if tags_fetched % 10 == 0 or tags_fetched == total_tags:
-                        logger.info(
-                            "Progress: tags fetched and saved: %s/%s",
-                            tags_fetched,
-                            total_tags,
-                        )
+        except Exception as e:
+            logger.error(
+                "Failed to fetch inital tags page. Aborting process. Error: %s", e
+            )
+            return
 
+        if total_tags > PAGE_SIZE:
+            total_pages: int = math.ceil(total_tags / PAGE_SIZE)
+            logger.info("Total pages to fetch: %s", total_pages)
+
+            tasks: list = []
+            for page in range(2, total_pages + 1):
+                tasks.append(fetch_tags_page(session, page, semaphore))
+
+            page_results = await asyncio.gather(*tasks)
+
+            for result_list in page_results:
+                all_results.extend(result_list)
+
+        else:
+            logger.info("Only one page of results found.")
+
+    # region ------------ Save data to file ------------
     end_time = time.time()
     elapsed_time = end_time - start_time
 
+    time_now: str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename: str = DATA_LOCAL / f"rawg_tags_response_{time_now}.json"
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(all_results, f, ensure_ascii=False, indent=2)
+
     logger.info("Process finished!")
-    logger.info("Total tags successfully fetched: %s", tags_fetched)
-    logger.info("Saved consolidated JSON Lines response to %s", filename)
+    logger.info(
+        "Fetched %s tags!",
+        len(all_results),
+    )
+    logger.info("Saved RAWG response to %s", filename)
     logger.info(
         "Elapsed time: seconds - %.2f  / minutes - %.2f / hours - %.2f",
         elapsed_time,
@@ -192,10 +189,9 @@ async def main():
         elapsed_time / 3600,
     )
     logger.info(
-        "----- ENDED FETCHER ROUTINE at %s -----",
+        "----- ENDED TAGS FETCHER ROUTINE at %s -----",
         datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     )
-    # endregion
 
 
 if __name__ == "__main__":
