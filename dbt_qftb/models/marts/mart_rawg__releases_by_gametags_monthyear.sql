10dedbfdd052004251f56d15eacf1f8ce1d91489
krn <contadorkrn@gmail.com>
2026-05-18 11:12:54 -0300

    FIX: Change name of column - tag_name_capitalized / FEAT: Add classification for top 10 tags

--- dbt_qftb/models/marts/mart_rawg__releases_by_gametags_monthyear.sql
+++ dbt_qftb/models/marts/mart_rawg__releases_by_gametags_monthyear.sql
@@ -6,10 +6,10 @@ game_tags as (
 
 group_by_date as (
     select
-        tag_name as game_tag,
+        tag_name_capitalized as game_tag,
 
         --- agregations
-        COUNT(tag_name) as tag_count,
+        COUNT(tag_name_capitalized) as tag_count,
 
         --- numerics
         EXTRACT(year from game_date_released) as extracted_year,
@@ -44,6 +44,13 @@ classify_tags_by_rank as (
             when rank = 1 then 'Top 1'
             when rank = 2 then 'Top 2'
             when rank = 3 then 'Top 3'
+            when rank = 4 then 'Top 4'
+            when rank = 5 then 'Top 5'
+            when rank = 6 then 'Top 6'
+            when rank = 7 then 'Top 7'
+            when rank = 8 then 'Top 8'
+            when rank = 9 then 'Top 9'
+            when rank = 10 then 'Top 10'
             else 'Other Tags'
         end as rank_category
     from ranked_tags
