9245d3534f759d37d836016719e2f8a0439b9a83
krn <contadorkrn@gmail.com>
2026-05-22 11:05:36 -0300

    FEAT: Include games table in the layer to get more data / STYLE: Style the code for better formatting and readability

--- dbt_qftb/models/intermediate/int_rawg__game_platforms_unnested.sql
+++ dbt_qftb/models/intermediate/int_rawg__game_platforms_unnested.sql
@@ -1,10 +1,20 @@
 with
 
+games as (
+    select
+        game_id,
+        game_name,
+        game_date_released
+
+    from {{ ref("stg_rawg__games") }}
+),
+
 game_details as (
     select
         game_id,
 
         unnest(platform_id) as platform_id
+
     from {{ ref("stg_rawg__game_details") }}
 ),
 
@@ -14,6 +24,7 @@ platforms as (
         platform_id,
         parent_platform_name,
         platform_name
+
     from {{ ref("int_rawg__parent_platform_unnested") }}
 ),
 
@@ -25,11 +36,18 @@ joined_games_and_platforms as (
         p.platform_id,
 
         -- strings
+        g.game_name,
         p.parent_platform_name,
-        p.platform_name
+        p.platform_name,
+
+        -- dates
+        g.game_date_released
 
     from
         game_details as gd
+
+    left join games as g
+        using (game_id)
 
     left join
         platforms as p
